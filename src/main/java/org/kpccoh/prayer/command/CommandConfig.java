af898a2fe15c55dfd3569cc20ef508fe10cb448e
KWONS <KWONS@JFSLT392660.msds.jfs.ohio.gov>
2016-05-19 14:27:46 -0400

    adds a test datasource for testing

--- src/main/java/org/kpccoh/prayer/command/CommandConfig.java
+++ src/main/java/org/kpccoh/prayer/command/CommandConfig.java
@@ -1,10 +1,22 @@
 package org.kpccoh.prayer.command;
 
+
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 
 @Configuration
 @ComponentScan
 public class CommandConfig {
+	
+	@Bean(destroyMethod="shutdown")
+	public DataSource dataSource() {
+		return new EmbeddedDatabaseBuilder()
+				.addScript("classpath:schema.sql")
+				.addScript("classpath:test-data.sql")
+				.build();
+	}
 }
